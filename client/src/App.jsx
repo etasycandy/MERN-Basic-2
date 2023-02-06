361068e4239227837e074621fc2fd52942025d54
Trần Đình Hoàng <super.royal147@gmail.com>
2023-02-07 02:42:13 +0700

    Setup React-icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import "./assets/sass/index.scss";
+import { BiBot } from "react-icons/bi";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-center mt-5 uppercase cursor-pointer hover:text-blue-500">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold text-center mt-5 uppercase cursor-pointer hover:text-blue-500">
+        Hello world!
+      </h1>
+      <div className="flex justify-center items-center w-screen">
+        <BiBot
+          className="text-9xl hover:text-green-500 cursor-wait"
+          size="5em"
+        />
+      </div>
+    </>
   );
 }
 
